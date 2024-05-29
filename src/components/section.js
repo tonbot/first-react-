1c521460f455c78d160a9424496d96f1393edd82
tonbot <toy505050@yahoo.com>
2024-05-29 16:26:05 +0100

    my first commit

---
+++ src/components/section.js
@@ -0,0 +1,10 @@
+import React from 'react';
+export default function Section(){
+    return (
+        <section>
+            <div className='text-center py-5'>
+                <h1>This is my first react app</h1>
+            </div>
+        </section>
+    )
+}
